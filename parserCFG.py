a959cfbda878e5181d3d2cc9d5e7b916968cd974
asyifashfr <16521132@mahasiswa.itb.ac.id>
2022-11-25 22:31:54 +0700

    update parser CFG

--- parserCFG.py
+++ parserCFG.py
@@ -1,36 +1,39 @@
-# ATURAN MENULIS GRAMMAR:
+# CATATAN:
 # Head pertama akan menjadi Start Symbol
-# Tidak mengandung Useless Production (tidak terdapat Variables yang tidak dapat diderivasi menjadi string dan tidak terdapat Productions yang tidak muncul pada derivasi string)
-# Tidak mengandung Null Production (tidak terdapat Variables yang menghasilkan epsilon, kecuali pada Start Symbol)
+# Tidak mengandung Useless Production (tidak terdapat Variables yang tidak dapat diderivasi 
+# menjadi string dan tidak terdapat Productions yang tidak muncul pada derivasi string)
+# Tidak mengandung Null Production (tidak terdapat Variables yang menghasilkan epsilon, 
+# kecuali pada Start Symbol)
 
-def read_grammar(nama_file):
-    file = open(nama_file, "r")
+def readGrammar(fileName):
+    file = open(fileName, "r")
     cfg = {}
 
-    baris = file.readline()
-    while baris != "":
-        head, body = baris.replace("\n", "").split(" -> ")
+    row = file.readline()
+    while row != "":
+        head, body = row.replace("\n", "").split(" -> ")
         
         if head not in cfg.keys():
             cfg[head] = [body.split(" ")]
         else:
             cfg[head].append(body.split(" "))
 
-        baris = file.readline()
+        row = file.readline()
 
     file.close()
 
     return cfg
 
 def isTerminal(string):
-    list_of_terminal = [
+    listTerminal = [
         "VAR",
         "LET",
+        "INCR",
+        "DECR",
         "CONTINUE",
         "BREAK",
         "STRING",
         "TITIK",
-        "FORMAT",
         "KBKI",
         "KBKA",
         "CONST",
@@ -45,21 +48,17 @@ def isTerminal(string):
         "DIV",
         "MOD",
         "POW",
-        "FLOORDIV",
         "NOT",
         "THROW",
         "DELETE",
         "ISEQ",
         "LEQ",
+        "L",
         "NEQ",
         "GEQ",
+        "G",
         "AND",
         "OR",
-        "XOR",
-        "LEFTSHIFT",
-        "RIGHTSHIFT",
-        "UNRIGHTSHIFT",
-        "WITH",
         "KKKI",
         "KKKA",
         "KARTITIK",
@@ -67,7 +66,6 @@ def isTerminal(string):
         "TITIKDUA",
         "EQ",
         "INT",
-        "CONSTRUCTOR",
         "IF",
         "ELSE",
         "ADDAS",
@@ -82,13 +80,9 @@ def isTerminal(string):
         "TRY",
         "CATCH",
         "FINALLY",
-        "CLASS",
         "WHILE",
         "DO",
         "FOR",
-        "IMPORT",
-        "FROM",
-        "RANGE",
         "NEWLINE",
         "NULL",
         "TRUE",
@@ -100,13 +94,7 @@ def isTerminal(string):
         "RETURN"
     ]
     
-    return string in list_of_terminal
+    return string in listTerminal
 
 def isVar(string):
     return not isTerminal(string)
-
-# def is_variables(string):
-#     return len(string) > 0 and string[0].isupper()
-
-# def is_terminal(string):
-#     return not is_variables(string)
